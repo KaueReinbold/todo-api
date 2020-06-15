bb0957b8ad0d12b363914651ca140fd3595fded0
Kaue Reinbold <kaue.reinbold@outlook.com>
2020-06-15 19:41:25 -0300

    Apply middleware to logger request insights

--- server.ts
+++ server.ts
@@ -4,11 +4,16 @@ import {
 } from 'https://deno.land/x/oak@v5.2.0/mod.ts';
 import { green, yellow } from 'https://deno.land/std@0.53.0/fmt/colors.ts';
 
-import todoRouter from './routes/todo.ts';
+import logger from './middlewares/logger.ts';
 import notFound from './middlewares/notFound.ts';
+
+import todoRouter from './routes/todo.ts';
 
 const app = new Application();
 const listenOptions: ListenOptions = { port: 8080 };
+
+app.use(logger.logger);
+app.use(logger.responseTime);
 
 app.use(todoRouter.routes());
 app.use(todoRouter.allowedMethods());
