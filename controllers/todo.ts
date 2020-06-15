477eb093b1bf2571cd03486bb9db401e10de2839
Kaue Reinbold <kaue.reinbold@outlook.com>
2020-06-15 17:50:42 -0300

    Get all todos

--- controllers/todo.ts
+++ controllers/todo.ts
@@ -1,5 +1,24 @@
+import {
+  Request,
+  Response,
+  RouteParams,
+} from 'https://deno.land/x/oak@v5.2.0/mod.ts';
+
+import todos from '../stubs/todos.ts';
+
 export default {
-  getAll: () => {},
+  getAll: ({
+    request,
+    response,
+    params,
+  }: {
+    request: Request;
+    response: Response;
+    params: RouteParams;
+  }) => {
+    response.status = 200;
+    response.body = todos;
+  },
   create: async () => {},
   getById: () => {},
   update: async () => {},
