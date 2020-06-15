ac55842e342ffaaa27af857c9fe739ba4ac5e3c7
Kaue Reinbold <kaue.reinbold@outlook.com>
2020-06-15 20:32:55 -0300

    Use dotenv to manage environment variables

---
+++ environment.ts
@@ -0,0 +1,8 @@
+import { config } from 'https://deno.land/x/dotenv/mod.ts';
+
+const environment = config();
+
+export default {
+  MYSQL_PASSWORD: environment.MYSQL_PASSWORD,
+  PORT: environment.PORT,
+};
