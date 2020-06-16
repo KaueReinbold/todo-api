816e7f31604e8ff149a6056afae1fe527bba7c1d
Kaue Reinbold <kaue.reinbold@outlook.com>
2020-06-15 21:32:02 -0300

    Query to check if reacord exists

--- repositories/todoRepository.ts
+++ repositories/todoRepository.ts
@@ -3,7 +3,15 @@ import client, { TABLE, DATABASE_NAME } from '../database/client.ts';
 import ITodo from '../interfaces/ITodo.ts';
 
 export default {
-  exists: async ({ id }: ITodo) => {},
+  exists: async ({ id }: ITodo) => {
+    const [
+      result,
+    ] = await client.query(
+      `SELECT COUNT(*) count FROM ${TABLE.TODO} WHERE id = ? LIMIT 1`,
+      [id]
+    );
+    return result.count > 0;
+  },
   getAll: async () => {
     const todos = await client.query(`SELECT * FROM ${TABLE.TODO}`);
 
