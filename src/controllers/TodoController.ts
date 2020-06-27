9cacba6ed1dbaad8fb3603e7f5b83ccb72227d13
Kaue Reinbold <kaue.reinbold@outlook.com>
2020-06-27 15:27:56 -0300

    Convert controller o be injectable

---
+++ src/controllers/TodoController.ts
@@ -0,0 +1,49 @@
+import { Service } from 'https://deno.land/x/di/mod.ts';
+
+import ITodo from '../interfaces/ITodo.ts';
+import TodoRepository from '../repositories/TodoRepository.ts';
+
+export interface IController {
+  getAll(): Promise<ITodo[] | null>;
+}
+
+@Service()
+class TodoController implements IController {
+  async getAll(): Promise<ITodo[] | null> {
+    const todos = [{ id: '1', title: 'string', isCompleted: true }];
+
+    return todos as ITodo[] | null;
+  }
+  // create: async (todo: ITodo): Promise<ITodo | null> => {
+  //   todo.isCompleted = false;
+
+  //   todo = await TodoRepository.add(todo);
+
+  //   return todo;
+  // },
+  // getById: async (id: number): Promise<ITodo | null> => {
+  //   let todoExists = await TodoRepository.exists(id);
+
+  //   if (!todoExists) return null;
+
+  //   const todo = await TodoRepository.get(id);
+
+  //   return todo;
+  // },
+  // update: async (id: number, todo: ITodo): Promise<ITodo | null> => {
+  //   todo = await TodoRepository.update(id, todo);
+
+  //   return todo;
+  // },
+  // delete: async (id: number): Promise<number | null> => {
+  //   const todoExists = await TodoRepository.exists(id);
+
+  //   if (!todoExists) return null;
+
+  //   await TodoRepository.delete(id);
+
+  //   return id;
+  // },
+}
+
+export default TodoController;
