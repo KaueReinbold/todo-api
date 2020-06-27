a8a8c6e2c798c3d4a4204a07f51f934a632dc87d
Kaue Reinbold <kaue.reinbold@outlook.com>
2020-06-27 15:29:41 -0300

    User startup to initialize depencies

--- server.ts
+++ server.ts
@@ -1,15 +1,17 @@
-import {
-  Application,
-  ListenOptions,
-} from 'https://deno.land/x/oak@v5.2.0/mod.ts';
+import 'https://cdn.pika.dev/@abraham/reflection@^0.7.0';
+
+import { Application, ListenOptions } from 'https://deno.land/x/oak/mod.ts';
 import { green, yellow } from 'https://deno.land/std@0.53.0/fmt/colors.ts';
 
 import environment from './environment.ts';
 
-import logger from './middlewares/logger.ts';
-import notFound from './middlewares/notFound.ts';
+import logger from './src/middlewares/logger.ts';
+import notFound from './src/middlewares/notFound.ts';
+import { RegisterService } from './startup.ts';
 
-import TodoRouter from './routes/TodoRouter.ts';
+RegisterService();
+
+import TodoRouter from './src/routes/TodoRouter.ts';
 
 const app = new Application();
 const listenOptions: ListenOptions = { port: environment.PORT };
